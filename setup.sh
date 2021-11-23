311965c0b7c5d9f37823797e7b8c2a0ca8de13c6
Mike van den Brink <31769246+MVDB0110@users.noreply.github.com>
2021-11-23 15:16:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,50 @@
 #!/bin/bash
 
-cd $1
+read -p "Where must CTFd be installed? (absolute or relative path is accepted.): " path
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sudo apt install -y python3-virtualenv
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  pip install virtualenv
+absolute_path=$(realpath $path)
+
+if [ -d $path ]
+then
+    echo "Directory $absolute_path exists."
+    cd $path
+else
+    echo "Error: Directory $absolute_path doesn't exist."
+    exit 1
 fi
 
-git clone https://github.com/CTFd/CTFd.git
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "Linux found"
+  echo "Installing virtualenv for Debian based systems"
+  sudo apt install -y python3-virtualenv > /dev/null
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "MacOS found"
+  echo "Installing virtualenv for MacOS"
+  pip install virtualenv > /dev/null
+else
+  echo "Error: No compatible OS found."
+  exit 1
+fi
+
+echo "--Installing CTFd--"
+echo "Cloning official CTFd"
+git clone https://github.com/CTFd/CTFd.git > /dev/null
 cd ./CTFd
-virtualenv env/
+echo "Creating virtual environment"
+virtualenv env/ > /dev/null
 source ./env/bin/activate
-pip install -r ./requirements.in
+echo "Installing pip requirements"
+pip install -r ./requirements.in > /dev/null
 
 cd ./CTFd/plugins/
-git clone https://github.com/MVDB0110/CTFd-Kubernetes
+echo "--Installing Kubernetes plugin--"
+echo "Cloning Kubernetes plugin"
+git clone https://github.com/MVDB0110/CTFd-Kubernetes > /dev/null
 mv CTFd-Kubernetes kubernetes
 rm -rf ./kubernetes/.git
-pip install -r kubernetes/requirements.txt
+echo "Installing Kuberntes pip requirements"
+pip install -r kubernetes/requirements.txt > /dev/null
 
 cd ../../
+echo "Starting CTFd"
 flask run -h 0.0.0.0
