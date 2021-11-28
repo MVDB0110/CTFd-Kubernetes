c089d6724d903eca61e41e48b693aa4042677c31
Mike van den Brink <31769246+MVDB0110@users.noreply.github.com>
2021-11-28 22:47:38 +0100

    Update base64.sh

--- base64.sh
+++ base64.sh
@@ -1,6 +1,9 @@
-export REGISTRY_USERNAME=$1
-export REGISTRY_PASSWORD=$2
-export REGISTRY_EMAIL=$3
+#!/bin/bash
+#Syntax: ./base64.sh registry-url username password email
+export REGISTRY_URL=$1
+export REGISTRY_USERNAME=$2
+export REGISTRY_PASSWORD=$3
+export REGISTRY_EMAIL=$4
 export BASE64_CREDENTIALS=$(echo "$REGISTRY_USERNAME:$REGISTRY_PASSWORD" | base64 -w 0)
-export BASE64_DOCKERCONFIGJSON=$(echo "{\"auths\": {\"https://registry.gitlab.com\": {\"username\": \"$REGISTRY_USERNAME\", \"password\": \"$REGISTRY_PASSWORD\", \"email\": \"$REGISTRY_EMAIL\", \"auth\": \"$BASE64_CREDENTIALS\"}}}" | base64 -w 0)
+export BASE64_DOCKERCONFIGJSON=$(echo "{\"auths\": {\"$REGISTRY_URL\": {\"username\": \"$REGISTRY_USERNAME\", \"password\": \"$REGISTRY_PASSWORD\", \"email\": \"$REGISTRY_EMAIL\", \"auth\": \"$BASE64_CREDENTIALS\"}}}" | base64 -w 0)
 echo $BASE64_DOCKERCONFIGJSON
